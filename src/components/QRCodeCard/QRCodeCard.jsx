128a701e3dae0de5a6f4a3530835459dd2a8d371
clément perreaut <clement.perreaut@diiage.org>
2023-04-20 23:16:12 +0200

    🚧 Fix html warning

--- src/components/QRCodeCard/QRCodeCard.jsx
+++ src/components/QRCodeCard/QRCodeCard.jsx
@@ -8,7 +8,7 @@ function QRCodeCard() {
     <div className='qr-code-container'>
       <img src={QR_CODE} alt='qr code' width={288} height={288} />
       <div className='qr-code-subcontainer'>
-        <h3>Improve your front-end skills by building projects</h3>
+        <h2>Improve your front-end skills by building projects</h2>
         <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
       </div>
     </div>
