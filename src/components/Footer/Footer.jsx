4ffa6e5741f00812b606b49fe36f2986d91a171e
clément perreaut <clement.perreaut@diiage.org>
2023-04-20 22:58:44 +0200

    💄 add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+  return (
+    <footer>
+      <p>Challenge by <a href='https://www.frontendmentor.io'>Frontend Mentor</a>. Coded by <a href='https://www.frontendmentor.io/profile/ClemFRontend'>ClemFRontend</a></p>
+    </footer>
+  )
+}
+
+export default Footer
